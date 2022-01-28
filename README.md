212bfaa076700e4668e3129e0157f866169e7396
uber19 <justinprood@gmail.com>
2022-01-27 18:28:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Site Backend
+
+A strapi application to manage content for a video portfolio website
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8992b096-471f-4dd9-aa0b-215b42905f61'
+};
